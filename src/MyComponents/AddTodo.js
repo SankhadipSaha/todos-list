e77c06ba1a347339a83d64e62e12a3b5f686f4f5
SankhadipSaha <sankhadipsaha04@gmail.com>
2025-06-22 21:57:30 +0530

    Task view Filters

--- src/MyComponents/AddTodo.js
+++ src/MyComponents/AddTodo.js
@@ -19,7 +19,7 @@ export const AddTodo = ({addTodo}) => {
   
   return (
     <div className='container my-3'>
-      <h3>Add a Todo</h3>
+      <h3>Add a Task</h3>
       <form onSubmit={submit}>
         <div className="mb-3">
           <label htmlFor="title" className="from-label " style={{ fontWeight: '600',fontSize:"20px",margin:"3px" }}>Title:</label>
@@ -31,7 +31,7 @@ export const AddTodo = ({addTodo}) => {
           <input type="text" value={desc} placeholder='Input your Description' onChange={(e)=> setDesc(e.target.value)}
           style={{fontWeight:"400",fontSize:"17px"}} className='form-control' id="desc" />
         </div>
-        <button type="submit" className='btn btn-bg btn-success'>Add Todo</button>
+        <button type="submit" className='btn btn-bg btn-success'>Add Task</button>
       </form>
       <hr />  
     </div>
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import { TodoItems } from './TodoItems'
+import React from 'react';
+import { TodoItems } from './TodoItems';
+import {useState} from 'react';
 
 
 export const Todos = (props) => {
+  const [view, setView] = useState("all");
+
+
+  const filteredTodos = props.todos.filter(todo => {
+  if (view === "active") return !todo.isCompleted;
+  if (view === "completed") return todo.isCompleted;
+  return true; 
+});
+
+  let headingText = "";
+  if (view === "all") headingText = "All Tasks";
+  else if (view === "active") headingText = "Active Tasks";
+  else if (view === "completed") headingText = "Completed Tasks";
+
 
   return (
     <div className="container" >
-        <h3  className="text-center my-3">Todos List</h3>
-        {props.todos.length===0? "No Todos to display":
-            props.todos.map((todo)=>{
+        <h3  className="text-center my-3">Tasks List</h3>
+        <div className="btn-group">
+          <button className="btn btn-dark m-3 rounded-3" onClick={() => setView("all")}>All</button>
+          <button className="btn btn-primary m-3 rounded-3" onClick={() => setView("active")}>Active</button>
+          <button className="btn btn-success m-3 rounded-3" onClick={() => setView("completed")}>Completed</button>
+        </div>
+        
+        <h3>{headingText}</h3>
+
+        {props.todos.length===0? "No Task to display":
+            filteredTodos.map((todo)=>{
             return <TodoItems todo={todo} key={todo.id} onDelete={props.onDelete}
              onToggle={props.handleToggle}/>
             })
